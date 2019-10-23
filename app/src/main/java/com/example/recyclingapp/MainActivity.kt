997a33b48e24bd7e73a04b776c92c53431b3ea78
thurle1 <thurle1@students.towson.edu>
2019-10-23 15:15:21 -0400

    Finished most of the layout

--- app/src/main/java/com/example/recyclingapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclingapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.recyclingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scan_btn.setOnClickListener { scan_btn_click() }
+        search_btn.setOnClickListener { search_btn_click() }
     }
+    private fun scan_btn_click(){
+        val intent = Intent(this, ScanCameraActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun search_btn_click(){
+        val intent = Intent(this, SearchActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
---
+++ app/src/main/java/com/example/recyclingapp/ScanCameraActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanCameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_camera)
+    }
+}
